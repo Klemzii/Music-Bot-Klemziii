32dc411dce84b60eed40dd066d6891f42176fb46
Klemzii <163579368+Klemzii@users.noreply.github.com>
2024-04-22 16:49:10 +0200

    Update animecommands.js

--- basicCommands/animecommands.js
+++ basicCommands/animecommands.js
@@ -9,7 +9,7 @@ module.exports = {
       .setTitle('Anime Commands')
       .setDescription(`__**✅ List Of Availabe Interations**__\n\n▶️ __**Section 1 :**__\n  blush, cuddle, dance, slap, bonk, bully, hug, confused, kiss, pat, happy, smile.\n\n▶️ __**Section 2 :**__\n yes, highfive, wink, wave, thinking, sad, cry, stare, bored, scream, nervous, kill.\n`)
       
-      .setImage(`https://cdn.discordapp.com/attachments/1140841446228897932/1142126954775068762/pxfuel.jpg`);
+      .setImage(`https://cdn.discordapp.com/attachments/1230753774096875562/1231978724044570625/Snitch_Town_Music.png?ex=6638ec98&is=66267798&hm=02bcd83dd70321be829a133c3e8a217797dfd62a279427a41d301ccbf34469cc&`);
     message.reply({ embeds: [embed] });
   },
 };
